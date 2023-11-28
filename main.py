20ad04ba60a1731f89dba86584a4e3e2e0668134
samba-py <90529499+samba-py@users.noreply.github.com>
2023-11-28 17:54:08 -0300

    v2
    request de genres names

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # en el main corro todas las funciones 
 from requests_api import get_movies_data
+from requests_api import get_genre_numbers
 from limpieza_datos import clean_data
 #from eda import visualize_data
 
@@ -7,11 +8,12 @@ api_key = "b2baa22e7bb745f2e4885c9c88051d08"
 
 # Obtengo datos de la API
 raw_data = get_movies_data(api_key)
-
+genre_id_name_converter = get_genre_numbers()
+print(genre_id_name_converter)
 # Realiza la limpieza de datos
 
 cleaned_data = clean_data(raw_data)
-print(cleaned_data["genre_ids"].head(10))
+
 
 
 # R3ealiza el análisis exploratorio de datos y visualizacion
--- requests_api.py
+++ requests_api.py
@@ -11,3 +11,19 @@ def get_movies_data(api_key):
     data = response.json()
     return data["results"]
 
+def get_genre_numbers():
+    url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+
+    headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmJhYTIyZTdiYjc0NWYyZTQ4ODVjOWM4ODA1MWQwOCIsInN1YiI6IjY1NjU1MTJiMTU2Y2M3MDEwY2IzMDdmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cmkm6KkxfZ6aC88PWLwgN23ypsl-yCtja6LnOhutFXI"}
+
+    response = requests.get(url, headers=headers)
+    genre_id_text = response.json()
+
+
+    print(response.text)
+    print(genre_id_text)
+    return response
+
+
